7e210f88b526f8f26f9c768aa5ea487357d76a7a
Sean Dou <douguohui@gmail.com>
2015-04-23 10:29:52 +0800

    1. Integrate github info; 2. Format output info

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+1.0.0 / 2015-4-23
+------------------
+* Format output info.
+* Integrate github info.
+
 0.0.1 / 2015-4-22
 ------------------
 * Initial release.
--- bin/cli.js
+++ bin/cli.js
@@ -20,20 +20,48 @@ var printHelp = function() {
   process.exit(0);
 };
 
-if (process.argv.length < 3) {
-  printHelp();
-}
+var printNpmInfo = function(info) {
+  console.log('');
+  console.log(info.name);
+  console.log('--------------------------------------------------');
+  console.log(info.description);
+  console.log('--------------------------------------------------');
+  console.log('homepage: ' + info.homepage);
 
-var spinner = new Spinner('%s');
-spinner.setSpinnerString('|/-\\');
-spinner.start();
+  if (info.github && (info.github != info.homepage)) {
+    console.log('github: ' + info.github)
+  }
+  
+  console.log('latest-version: ' + info['latest-version']);
+  console.log('modified: ' + info.modified);
+  
+  if (info.forks) {
+    console.log('watchers: ' + info.watchers + ', stars: ' + info.stars + ', forks: ' + info.forks);
+  }
+  
+  console.log('--------------------------------------------------');
+  console.log('');
+};
 
-npminfo(process.argv[2], function(err, info) {
-  spinner.stop('clean');
-
-  if (err) {
-    console.error(err);
-    process.exit(1);
+function main() {
+  if (process.argv.length < 3) {
+    printHelp();
   }
 
-});
+  var spinner = new Spinner('%s');
+  spinner.setSpinnerString('|/-\\');
+  spinner.start();
+
+  npminfo(process.argv[2], function(err, info) {
+    spinner.stop('clean');
+
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    printNpmInfo(info);
+  });
+}
+
+main();
